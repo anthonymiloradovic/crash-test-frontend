0b691a7461e4daf0423f6da89156018b72fac1ef
anthonymiloradovic <anthony.miloradovic@icloud.com>
2023-03-07 14:14:13 +0100

    +++

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,27 +1,32 @@
-import { ParallaxBanner, Parallax } from 'react-scroll-parallax';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const Effect = () => {
   return (
-    <ParallaxBanner
-      layers={[
-        {
-          image: '/bg-parallax.jpg',
-          amount: 0.5,
-        },
-        {
-          image: '/building2.png',
-          amount: 0.2,
-          slowerScrollRate: true,
-          expand: 0.05,
-          children: <div style={{ height: '100vh' }} />,
-        },
-      ]}
-      style={{ height: '100vh' }}
-    >
-      <Parallax disabled>
-        <div style={{ height: '100vh' }} />
+    <div>
+      <Parallax pages={4}>
+        <ParallaxLayer
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: 'url("/burjkhalifa.png")',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center',
+            position: 'absolute',
+          }}
+        />
+        <ParallaxLayer
+          speed={0.5}
+          factor={2}
+          style={{
+            backgroundImage: 'url("/building.png")',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center',
+            
+            position: 'absolute',
+          }}
+        />
       </Parallax>
-    </ParallaxBanner>
+    </div>
   );
 };
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Effect from '../components/Effect';
-import { ParallaxProvider } from 'react-scroll-parallax';
 
 const About = () => {
     return (
 
         <div>
-            <Navbar/>
-            <ParallaxProvider>
+
+           
            <Effect/>
-           </ParallaxProvider>
+          
         </div>
     );
 };
